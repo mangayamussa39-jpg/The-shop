557d169d1ef88bee86a5922ba4fd7e58ed0512ba
mangayamussa39-jpg <mangayamussa39@gmail.com>
2026-06-07 10:03:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
 // sw.js
 self.addEventListener('install', (event) => {
+  // Forces the waiting service worker to become the active service worker immediately
   self.skipWaiting(); 
+  console.log('Bakery Service Worker installed.');
+});
+
+self.addEventListener('activate', (event) => {
+  // Allows the service worker to take control of the page immediately
+  event.waitUntil(self.clients.claim());
+  console.log('Bakery Service Worker activated.');
 });
 
 self.addEventListener('fetch', (event) => {
-  // Static pages still need this handler to pass the install check
+  // This empty fetch handler is mandatory to trigger the installation prompt
   event.respondWith(fetch(event.request));
 });
