0a4af5c2fd829ba42409beca60f8325972887dcd
mangayamussa39-jpg <mangayamussa39@gmail.com>
2026-06-08 12:27:30 +0300

    Delete register-sw.js

--- register-sw.js
+++
@@ -1,9 +0,0 @@
-// register-sw.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // Removed the / from 'sw.js'
-    navigator.serviceWorker.register('sw.js')
-      .then(reg => console.log('Service Worker Active!', reg.scope))
-      .catch(err => console.log('Service Worker Error', err));
-  });
-}
